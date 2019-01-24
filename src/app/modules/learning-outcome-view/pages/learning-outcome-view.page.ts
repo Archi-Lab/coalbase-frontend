8fae066c266b64737cb75b4ed669fd9a5d6ccc5a
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2019-01-24 16:32:46 +0100

    inital learning outcome editor

--- src/app/modules/learning-outcome-view/pages/learning-outcome-view.page.ts
+++ src/app/modules/learning-outcome-view/pages/learning-outcome-view.page.ts
@@ -8,10 +8,28 @@ import {LearningOutcomeService} from "../../../core/services/learning-outcome.se
   styleUrls: ["./learning-outcome-view.page.scss"]
 })
 export class LearningOutcomeViewPage {
-
   learningOutcomes: LearningOutcome[] = [];
+  selectedLearningOutcome: LearningOutcome = {title: "", skill: {description: "", taxonomyLevel: 0}, toolKit: [], purpose: ""};
 
   constructor(private learningOutcomeService: LearningOutcomeService) {
-    learningOutcomeService.learningOutcomes.subscribe(learningOutcomes => this.learningOutcomes = learningOutcomes);
+    learningOutcomeService.learningOutcomes.subscribe(learningOutcomes => {
+      this.learningOutcomes = learningOutcomes;
+      if (this.learningOutcomes.length > 0) {
+        this.selectedLearningOutcome = this.learningOutcomes[0];
+      }
+    });
+  }
+  selectLearningOutcome(learningOutcome: LearningOutcome) {
+    if (learningOutcome.title
+        && learningOutcome.skill
+        && learningOutcome.skill.description
+        && learningOutcome.skill.taxonomyLevel
+        && learningOutcome.toolKit
+        && learningOutcome.toolKit.length > 0
+        && learningOutcome.purpose) {
+      this.selectedLearningOutcome = learningOutcome;
+    } else {
+      console.log("Selected LearningOutcome is not valid, there are missing attributes: " + JSON.stringify(learningOutcome));
+    }
   }
 }
