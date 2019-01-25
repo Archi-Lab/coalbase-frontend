305a86dd3a99669adcb0621882702c32fbb25f75
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2019-01-25 13:49:57 +0100

    format files to match tslint

--- src/app/modules/learning-outcome/learning-outcome-editor/learning-outcome-editor.component.ts
+++ src/app/modules/learning-outcome/learning-outcome-editor/learning-outcome-editor.component.ts
@@ -1,23 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {LearningOutcome} from "../../../shared/models/learning-outcome.model";
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
-import {switchMap} from 'rxjs/operators';
-import {LearningOutcomeService} from '../../../core/services/learning-outcome.service';
+import {ActivatedRoute} from "@angular/router";
+import {LearningOutcomeService} from "../../../core/services/learning-outcome.service";
 
 @Component({
   selector: "app-learning-outcome-editor",
   templateUrl: "./learning-outcome-editor.component.html",
   styleUrls: ["./learning-outcome-editor.component.scss"]
 })
-export class LearningOutcomeEditorComponent implements OnInit{
+export class LearningOutcomeEditorComponent implements OnInit {
   learningOutcome: LearningOutcome = {id: 0, title: "", skill: {description: "", taxonomyLevel: 0}, toolKit: [], purpose: ""};
 
   constructor(private route: ActivatedRoute, private learningOutcomeService: LearningOutcomeService) {
 
   }
+
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      if(params["learningOutcome"]){
+      if (params["learningOutcome"]) {
         this.learningOutcome = this.learningOutcomeService.learningOutcome(params["learningOutcome"]);
       } else {
         this.learningOutcome = this.learningOutcomeService.firstlearningOutcome;
--- src/app/modules/learning-outcome/pages/learning-outcome.page.ts
+++ src/app/modules/learning-outcome/pages/learning-outcome.page.ts
@@ -1,8 +1,8 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component} from "@angular/core";
 import {LearningOutcome} from "../../../shared/models/learning-outcome.model";
 import {LearningOutcomeService} from "../../../core/services/learning-outcome.service";
 import {Router} from "@angular/router";
-import {LearningOutcomeEditorComponent} from '../learning-outcome-editor/learning-outcome-editor.component';
+
 
 @Component({
   selector: "app-learning-outcome-view",
