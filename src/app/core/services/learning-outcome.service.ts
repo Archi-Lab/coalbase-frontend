3a2bce17092041a42bbe2861bc552b968380e968
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2019-01-28 13:33:01 +0100

    add new more complex examples for learning outcomes

--- src/app/core/services/learning-outcome.service.ts
+++ src/app/core/services/learning-outcome.service.ts
@@ -10,34 +10,52 @@ const learningOutcomes: LearningOutcome[] = [
       taxonomyLevel: 'SYNTHESIS'
     },
     tools: [
-      'das Makro- und Mikroumfeld des relevanten Marktes so wie das eigenen Unternehmen analysieren',
-      'Konsequenzen für die verschiedenen Bereiche der Marketingpolitik entwerfen'
+      'indem sie das Makro- und Mikro-Umfeld (insb. Kunden und Kaufverhalten)des relevanten Markts',
+      'so wie das eigene Unternehmen analysieren (auch mit Marktforschung), daraus die Elemente einer Marketingstrategie ableiten',
+      'und Konsequenzen für die verschiedenen Bereiche der Marketingpolitik entwerfen'
     ],
-    purpose: 'Produkte, Preise, Kommunikation und den Vertrieb bewusst marktorientiert zu gestalten'
+    purpose: 'um Produkte, Preise, Kommunikation und den Vertrieb bewusst marktorientiert zu gestalten.'
   },
   {
     id: '739d0737-419a-417d-8676-04ad691bf296',
     competence: {
-      action: 'Die Studierenden machen dinge',
-      taxonomyLevel: 'SYNTHESIS'
+      action: 'Die Studierenden können einen thematisch vorgegebenen funktionstüchtigen Spielprototypen kriteriengeleitet entwickeln,',
+      taxonomyLevel: 'APPLY'
     },
     tools: [
-      'das Makro- und Mikroumfeld des relevanten Marktes so wie das eigenen Unternehmen analysieren',
-      'Konsequenzen für die verschiedenen Bereiche der Marketingpolitik entwerfen'
+      'indem Game Development Environment (z.B. Unity 3D, Gamemaker) angewendet, Programmiersprachen (z.B. Javascript, C#) genutzt',
+      'und der Umgang mit Game Assets gelernt wird'
     ],
-    purpose: 'Produkte, Preise, Kommunikation und den Vertrieb bewusst marktorientiert zu gestalten'
+    purpose: 'um in Folgeveranstaltungen Spielprototypen mit wachsender Komplexität entwickeln zu können.'
   },
   {
     id: 'a613327b-2c9c-4361-b753-da7117a6c12b',
     competence: {
-      action: 'Die Studierenden machen dinge',
+      action: 'Die Studierenden können pharmazeutische Darreichungsformen herstellen',
       taxonomyLevel: 'SYNTHESIS'
     },
     tools: [
-      'das Makro- und Mikroumfeld des relevanten Marktes so wie das eigenen Unternehmen analysieren',
-      'Konsequenzen für die verschiedenen Bereiche der Marketingpolitik entwerfen'
+      'indem sie den regulatorischen Rahmen der geltenden Arzneibücher auslegen, ' +
+      'plausible Formulierungen der Auswahl geeigneter Hilfsstoffe zusammenstellen und ' +
+      'die technischen Details gängiger pharmazeutischer Herstellungsmethoden berücksichtigen,'
     ],
-    purpose: 'Produkte, Preise, Kommunikation und den Vertrieb bewusst marktorientiert zu gestalten'
+    purpose: 'um später im Praktikum stabile Arzneimittel für chemische und biologische Wirkstoffmoleküle herzustellen.'
+  },
+  {
+    id: 'd373ff5d-6681-4d83-bb94-3a275ff82ecb',
+    competence: {
+      action: 'Die Studierenden können eine kriteriengeleitete (Eignung, Kosten, Energieverbrauch) Aggregatauswahl' +
+        'für einen vorgegebenen Kontext des Transports und Lagerns von Stoffen treffen,',
+      taxonomyLevel: 'SYNTHESIS'
+    },
+    tools: [
+      'indem sie tabellarisch Alternativen des Aggregats im Rahmen eines anwendungsbezogenen Stärken-/\n' +
+      'Schwächenprofils bewerten, ',
+      'die Vorauswahl durch Schätzroutinen und vorgestellte Formeln verifizieren',
+      'und dabei Gefahrenchecks bezüglich Staubexplosionen durchführen,'
+    ],
+    purpose: 'um eine verfahrenstechnisch optimale Geräteauswahl zu treffen, ' +
+      'Risiken zu minimieren und die Entscheidungen betrieblich durchsetzen zu können.'
   }
 ];
 
