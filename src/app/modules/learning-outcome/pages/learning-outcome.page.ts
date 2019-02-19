20f1f836fe40fcab4c2731fb791f7786b6fec5bf
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2019-02-19 15:19:19 +0100

    change subscribe to async pipe in learning outcome overview

--- src/app/modules/learning-outcome/pages/learning-outcome.page.ts
+++ src/app/modules/learning-outcome/pages/learning-outcome.page.ts
@@ -1,23 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {LearningOutcome} from '../../../shared/models/learning-outcome.model';
 import {Router} from '@angular/router';
 import {LearningOutcomeService} from '../../../core/services/learning-outcome/learning-outcome.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'applearningoutcomeview',
   templateUrl: './learning-outcome.page.html',
   styleUrls: ['./learning-outcome.page.scss']
 })
-export class LearningOutcomePage implements OnInit {
-  learningOutcomes: LearningOutcome[] = [];
+export class LearningOutcomePage {
+  learningOutcomes: Observable<LearningOutcome[]>;
 
   constructor(private learningOutcomeService: LearningOutcomeService, private router: Router) {
+    this.learningOutcomes = this.learningOutcomeService.getAll();
   }
 
-  ngOnInit(): void {
-    this.learningOutcomeService.getAll().subscribe((learningOutcomes: LearningOutcome[]) => {
-        this.learningOutcomes = learningOutcomes;
-      }
-    );
-  }
 }
