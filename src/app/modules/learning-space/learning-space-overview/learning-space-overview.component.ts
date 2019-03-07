e913fe00dbb3a2d3428203a18bb4bb8a69919c1f
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2019-03-07 18:19:30 +0100

    request real learningSpace data now

--- src/app/modules/learning-space/learning-space-overview/learning-space-overview.component.ts
+++ src/app/modules/learning-space/learning-space-overview/learning-space-overview.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {LearningSpace} from "../../../shared/models/learning-space.model";
-import {LearningOutcome} from "../../../shared/models/learning-outcome.model";
+import {LearningSpace} from '../../../shared/models/learning-space.model';
+import {LearningSpaceService} from '../../../core/services/learning-space/learning-space.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-learning-space-overview',
@@ -8,60 +9,59 @@ import {LearningOutcome} from "../../../shared/models/learning-outcome.model";
   styleUrls: ['./learning-space-overview.component.scss']
 })
 export class LearningSpaceOverviewComponent implements OnInit {
-
-  space1 = new LearningSpace("space1");
-  space2 = new LearningSpace("space2", new LearningOutcome({
-    action: "action1",
-    taxonomyLevel: "LEVEL1"
-  }, [{value: "tool1"}, {value: "tool2"}], {value: "purpose1"}), this.space1);
-  space3 = new LearningSpace("space3", new LearningOutcome({
-    action: "action1",
-    taxonomyLevel: "LEVEL1"
-  }, [{value: "tool1"}, {value: "tool2"}], {value: "purpose1"}), this.space2);
-  space4 = new LearningSpace("space4", new LearningOutcome({
-    action: "action1",
-    taxonomyLevel: "LEVEL1"
-  }, [{value: "tool1"}, {value: "tool2"}], {value: "purpose1"}), this.space3);
-  space5 = new LearningSpace("space5", new LearningOutcome({
-    action: "action1",
-    taxonomyLevel: "LEVEL1"
-  }, [{value: "tool1"}, {value: "tool2"}], {value: "purpose1"}), this.space4);
-  space6 = new LearningSpace("space6", new LearningOutcome({
-    action: "action1",
-    taxonomyLevel: "LEVEL1"
-  }, [{value: "tool1"}, {value: "tool2"}], {value: "purpose1"}), this.space5);
+  /*
+  space1 = new LearningSpace('space1');
+  space2 = new LearningSpace('space2', new LearningOutcome({
+    action: 'action1',
+    taxonomyLevel: 'LEVEL1'
+  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space1);
+  space3 = new LearningSpace('space3', new LearningOutcome({
+    action: 'action1',
+    taxonomyLevel: 'LEVEL1'
+  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space2);
+  space4 = new LearningSpace('space4', new LearningOutcome({
+    action: 'action1',
+    taxonomyLevel: 'LEVEL1'
+  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space3);
+  space5 = new LearningSpace('space5', new LearningOutcome({
+    action: 'action1',
+    taxonomyLevel: 'LEVEL1'
+  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space4);
+  space6 = new LearningSpace('space6', new LearningOutcome({
+    action: 'action1',
+    taxonomyLevel: 'LEVEL1'
+  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space5);
 
   sampleLearningSpaces: LearningSpace[] = [
     this.space2, this.space3, this.space6, this.space1, this.space4, this.space5
   ];
-
+*/
   sortedLearningSpaces: LearningSpace[] = [];
 
-  constructor() {
+  constructor(private learningSpaceService: LearningSpaceService, private router: Router) {
   }
 
   ngOnInit() {
-    this.space1._links = {};
-    this.space1._links.self = {};
-    this.space1._links.self.href = "http://test.io/123uahsdj1331";
-    this.sortedLearningSpaces = this.sortLearningSpaces(this.sampleLearningSpaces);
-  }
 
-  private sortLearningSpaces(unsortedLearningSpaces: LearningSpace[]): LearningSpace[] {
-    const sortedLearningSpaces: LearningSpace[] = [];
 
-    let index: number = 0;
-    while (index < unsortedLearningSpaces.length) {
-
-      if (sortedLearningSpaces.length === 0) {
-        sortedLearningSpaces.push(unsortedLearningSpaces.filter(learningSpace => learningSpace.isFirst())[0]);
-      } else {
-        sortedLearningSpaces.push(unsortedLearningSpaces.filter(learningSpace => learningSpace.isRequirement(sortedLearningSpaces[sortedLearningSpaces.length - 1]))[0]);
+    this.learningSpaceService.learningSpaces.subscribe(learningSpaces => {
+        learningSpaces.forEach(learningSpace => {
+          learningSpace.getRelation(LearningSpace, 'requirement').subscribe(
+            (requirement: LearningSpace) => {
+              learningSpace.requirement = requirement;
+              this.sortedLearningSpaces.push(learningSpace);
+            },
+            (error) => {
+              if (this.sortedLearningSpaces.length > 0 && this.sortedLearningSpaces[0].isFirst()) {
+                console.log('There is more than one learningSpace without requirement, ' +
+                  'this can happen if the backend reponds with an error');
+              } else {
+                this.sortedLearningSpaces.unshift(learningSpace);
+              }
+            }
+          );
+        });
       }
-      index++;
-    }
-
-    return sortedLearningSpaces;
+    );
   }
-
 }
