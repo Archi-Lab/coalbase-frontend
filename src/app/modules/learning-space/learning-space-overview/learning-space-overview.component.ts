7d331df1ad7359463e72069ff60a5c1ea5e9a269
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2019-03-08 09:21:20 +0100

    request learning Outcomes for learningSpaces

--- src/app/modules/learning-space/learning-space-overview/learning-space-overview.component.ts
+++ src/app/modules/learning-space/learning-space-overview/learning-space-overview.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {LearningSpace} from '../../../shared/models/learning-space.model';
 import {LearningSpaceService} from '../../../core/services/learning-space/learning-space.service';
 import {Router} from '@angular/router';
+import {LearningOutcome} from '../../../shared/models/learning-outcome.model';
 
 @Component({
   selector: 'app-learning-space-overview',
@@ -9,43 +10,17 @@ import {Router} from '@angular/router';
   styleUrls: ['./learning-space-overview.component.scss']
 })
 export class LearningSpaceOverviewComponent implements OnInit {
-  /*
-  space1 = new LearningSpace('space1');
-  space2 = new LearningSpace('space2', new LearningOutcome({
-    action: 'action1',
-    taxonomyLevel: 'LEVEL1'
-  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space1);
-  space3 = new LearningSpace('space3', new LearningOutcome({
-    action: 'action1',
-    taxonomyLevel: 'LEVEL1'
-  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space2);
-  space4 = new LearningSpace('space4', new LearningOutcome({
-    action: 'action1',
-    taxonomyLevel: 'LEVEL1'
-  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space3);
-  space5 = new LearningSpace('space5', new LearningOutcome({
-    action: 'action1',
-    taxonomyLevel: 'LEVEL1'
-  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space4);
-  space6 = new LearningSpace('space6', new LearningOutcome({
-    action: 'action1',
-    taxonomyLevel: 'LEVEL1'
-  }, [{value: 'tool1'}, {value: 'tool2'}], {value: 'purpose1'}), this.space5);
-
-  sampleLearningSpaces: LearningSpace[] = [
-    this.space2, this.space3, this.space6, this.space1, this.space4, this.space5
-  ];
-*/
   sortedLearningSpaces: LearningSpace[] = [];
 
   constructor(private learningSpaceService: LearningSpaceService, private router: Router) {
   }
 
   ngOnInit() {
-
-
     this.learningSpaceService.learningSpaces.subscribe(learningSpaces => {
         learningSpaces.forEach(learningSpace => {
+          learningSpace.getRelation(LearningOutcome, 'learningOutcome').subscribe(
+            (learningOutcome: LearningOutcome) => learningSpace.learningOutcome = learningOutcome
+          );
           learningSpace.getRelation(LearningSpace, 'requirement').subscribe(
             (requirement: LearningSpace) => {
               learningSpace.requirement = requirement;
