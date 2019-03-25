8eca8f81aaa24d483ccadbbfd73772c136575dcc
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2019-03-25 10:58:19 +0100

    create inital service

--- src/app/modules/course/pages/course.page.ts
+++ src/app/modules/course/pages/course.page.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {Course} from '../../../shared/models/course/course.model';
 import {LearningSpace} from '../../../shared/models/learning-space/learning-space.model';
 import {Observable, of} from 'rxjs';
+import {CourseService} from '../../../core/services/course/course.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'appcourseview',
@@ -12,8 +14,17 @@ export class CoursePage {
   private sampleCourses: Course[] = [new Course('Softwaretechnik 1', [new LearningSpace('Lernraum')], 'Wichtige Veranstaltung')];
   courses: Observable<Course[]>;
 
-  constructor() {
+  constructor(private courseService: CourseService, private router: Router, private route: ActivatedRoute) {
+    // TODO use service instead of sample data
     this.courses = of(this.sampleCourses);
+    this.redirectIfEmpty();
   }
 
+  private redirectIfEmpty() {
+    this.courses.subscribe(courses => {
+      if (courses.length === 0) {
+        this.router.navigate(['new'], {relativeTo: this.route});
+      }
+    });
+  }
 }
