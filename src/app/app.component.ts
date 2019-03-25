483f7ea308f52663e25421f0e9342a79a2e6a81a
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2019-03-25 09:17:11 +0100

    add logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {KeycloakService} from 'keycloak-angular';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CoalBase';
+
+  constructor(private keycloakservice: KeycloakService) {
+  }
+
+  logout() {
+    this.keycloakservice.logout(environment.keyCloak);
+  }
 }
