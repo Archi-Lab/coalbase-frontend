e503d9558bee8086993d200c9b6548ef84812caf
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-04-04 15:38:10 +0200

    add caching service and interceptor

---
+++ src/app/core/interceptors/cache/cache.interceptor.ts
@@ -0,0 +1,35 @@
+import {Injectable} from "@angular/core";
+import {CacheService} from "../../services/cache/cache.service";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from "@angular/common/http";
+import {Observable, of} from "rxjs";
+import {tap} from "rxjs/operators";
+
+@Injectable()
+export class CacheInterceptor implements HttpInterceptor {
+  constructor(private readonly cacheService: CacheService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (req.method.toLowerCase() === "get") {
+      const cachedResponse = this.cacheService.get(req);
+      return cachedResponse ? of(cachedResponse) : this.sendRequest(req, next);
+    } else {
+      this.cacheService.deleteAll();
+      return this.sendRequest(req, next);
+    }
+
+  }
+
+  sendRequest(
+    req: HttpRequest<any>,
+    next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      tap(event => {
+        if (event instanceof HttpResponse) {
+          this.cacheService.put(req, event);
+        }
+      })
+    );
+  }
+}
---
+++ src/app/core/services/cache/cache.service.ts
@@ -0,0 +1,71 @@
+import {Injectable} from "@angular/core";
+import {HttpRequest, HttpResponse} from "@angular/common/http";
+
+@Injectable({providedIn: "root"})
+export class CacheService {
+
+  private readonly cache: Map<string, CacheResponse>;
+  private readonly maxAge: number = 600000;
+
+  constructor() {
+    this.cache = new Map();
+  }
+
+  public get(req: HttpRequest<any>): HttpResponse<any> | undefined {
+    const url: string = this.getUrl(req);
+    const cacheResponse: CacheResponse | undefined = this.cache.get(url);
+
+    if (!cacheResponse) {
+      return undefined;
+    }
+
+    if (this.isExpired(cacheResponse)) {
+      this.cache.delete(url);
+      return undefined;
+    }
+    return cacheResponse.data;
+  }
+
+  public put(req: HttpRequest<any>, res: HttpResponse<any>): void {
+    const content_type: string = res.headers.get("Content-Type") as string;
+    if (content_type != undefined && content_type.includes("application/json")) {
+      const url: string = this.getUrl(req);
+      const expiresOn: number = Date.now() + this.maxAge;
+      this.cache.set(url, {expiresOn, data: res});
+    }
+  }
+
+  public deleteAll(): void {
+    this.cache.forEach((value, key) => this.cache.delete(key));
+  }
+
+  public delete(req: HttpRequest<any>): void {
+    const url: string = this.getUrl(req);
+    const cacheResponse: CacheResponse | undefined = this.cache.get(url);
+
+    if (cacheResponse) {
+      this.cache.delete(url);
+    }
+
+  }
+
+  private isExpired(cacheResponse: CacheResponse): boolean {
+    return cacheResponse.expiresOn <= Date.now();
+  }
+
+  private getUrl(req: HttpRequest<any>): string {
+    return this.trimUrl(req.urlWithParams)
+  }
+
+  private trimUrl(url: string): string {
+    if (url.charAt(url.length - 1) === '/') {
+      return url.substr(0, url.length - 1);
+    }
+    return url;
+  }
+}
+
+interface CacheResponse {
+  expiresOn: number,
+  data: HttpResponse<any>
+}
