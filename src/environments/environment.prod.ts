b736c7479d7bcc032644f607c5ab797014ce971f
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-05-03 16:36:10 +0200

    updates prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  keyCloak: 'https://login.coalbase.io/auth',
+  keyCloak: 'https://login.archi-lab.io/auth',
   realm: 'coalbase',
   clientID: 'coalbase',
-  coalbaseAPI: 'https://api.coalbase.io'
+  coalbaseAPI: 'https://api.coalbase.archi-lab.io/'
 };
