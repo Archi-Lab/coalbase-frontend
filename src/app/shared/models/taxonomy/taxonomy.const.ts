4afba97910b63844cb6681348197d5ee373c46bc
JannIntveen <JannIntveen@gmail.com>
2019-05-31 11:51:58 +0200

    Taxonomy Level is now displayed in Learning Space Overview

---
+++ src/app/shared/models/taxonomy/taxonomy.const.ts
@@ -0,0 +1,53 @@
+export const TAXONOMY_LEVELS = [
+  {
+    backend: 'KNOWLEDGE',
+    name: 'Wissen',
+    description: 'Die Lernenden geben wieder, was sie vorher gelernt haben.' +
+      ' Der Prüfungsstoff musste auswendig gelernt oder geübt werden.'
+  },
+  {
+    backend: 'COMPREHENSION',
+    name: 'Verständnis',
+    description: 'Die Lernenden erklären z.B. einen Begriff, eine Formel,' +
+      ' einen Sachverhalt oder ein Gerät. Ihr Verständnis zeigt sich darin,' +
+      ' dass sie das Gelernte auch in einem Kontext präsent haben, der sich ' +
+      'vom Kontext unterscheidet, in dem gelernt worden ist. So können die' +
+      ' Lernenden z.B. einen Sachverhalt auch umgangssprachlich erläutern' +
+      ' oder den Zusammenhang graphisch darstellen.'
+  },
+  {
+    backend: 'APPLICATION',
+    name: 'Anwendung',
+    description: 'Die Lernenden wenden etwas Gelerntes' +
+      ' in einer neuen Situation an.' +
+      ' Diese Anwendungssituation ist bisher nicht vorgekommen.'
+  },
+  {
+    backend: 'ANALYSIS',
+    name: 'Analyse',
+    description: 'Die Lernenden zerlegen Modelle,' +
+      ' Verfahren oder anderes in deren Bestandteile.' +
+      ' Dabei müssen sie in komplexen Sachverhalten die Aufbauprinzipien' +
+      ' oder inneren Strukturen entdecken. Sie erkennen Zusammenhänge.'
+  },
+  {
+    backend: 'EVALUATION',
+    name: 'Beurteilung',
+    description: 'Die Lernenden zeigen eine konstruktive Leistung.' +
+      ' Sie müssen verschiedene Teile zusammenfügen,' +
+      ' die sie noch nicht zusammen erlebt oder gesehen haben.' +
+      ' Aus ihrer Sicht müssen sie eine schöpferische Leistung erbringen.' +
+      ' Das Neue ist aber in der bisherigen Erfahrung' +
+      ' oder in der Kenntnis der Lernenden noch nicht vorhanden.'
+  },
+  {
+    backend: 'SYNTHESIS',
+    name: 'Synthese',
+    description: 'Die Lernenden zeigen eine konstruktive Leistung.' +
+      ' Sie müssen verschiedene Teile zusammenfügen,' +
+      ' die sie noch nicht zusammen erlebt oder gesehen haben.' +
+      ' Aus ihrer Sicht müssen sie eine schöpferische Leistung erbringen.' +
+      ' Das Neue ist aber in der bisherigen Erfahrung' +
+      ' oder in der Kenntnis der Lernenden noch nicht vorhanden.'
+  }
+];
