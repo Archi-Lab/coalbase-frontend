bc2b682c1d6debc12bc3d449a0a5b53656547e32
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-08-30 09:10:25 +0200

    Change API URL to coalbase2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   keyCloak: 'https://login.archi-lab.io/auth',
   realm: 'archilab',
   clientID: 'coalbase',
-  coalbaseAPI: 'https://api.coalbase.archi-lab.io/'
+  coalbaseAPI: 'https://api.coalbase2.archi-lab.io/'
 };
